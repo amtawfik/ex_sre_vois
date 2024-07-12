da9a8c88d98721ef9e861144c58964aa4106a392
Ahmed Elwakf <amtawfik140@gmail.com>
2024-07-12 22:20:10 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "5.58.0"
-        }
-    }
-}
-
-provider "aws" {
-    region = "us-west-2"
-}
-
 module "vpc" {
     source = "./modules/vpc"
     vpc_cidr = var.vpc_cidr
