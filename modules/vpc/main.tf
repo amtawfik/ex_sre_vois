497ad5a9eab0034d2a3f166d970942cdbf55a0e7
Ahmed Elwakf <amtawfik140@gmail.com>
2024-07-12 22:29:51 +0300

    Update vpc main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,21 +1,21 @@
-resource "aws_vpc" "this" {
+resource "aws_vpc" "myvpc" {
     cidr_block = var.vpc_cidr
 }
 
 resource "aws_subnet" "public" {
     availability_zone = "us-west-2a"
     cidr_block = var.public_subnet_cidr
-    vpc_id = aws_vpc.this.id
+    vpc_id = aws_vpc.myvpc.id
 }
 
 resource "aws_subnet" "private" {
     availability_zone = "us-west-2a"
     cidr_block = var.private_subnet_cidr
-    vpc_id = aws_vpc.this.id
+    vpc_id = aws_vpc.myvpc.id
 }
 
 output "vpc_id" {
-    value = aws_vpc.this.id
+    value = aws_vpc.myvpc.id
 }
 
 output "public_subnet_id" {
