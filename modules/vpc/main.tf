5ea4c8e9a266cb56fed53ff5d9e662d4c6f666f2
Ahmed Elwakf <amtawfik140@gmail.com>
2024-07-12 19:13:21 +0300

    Update vpc main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,39 +1,27 @@
-resource "aws_vpc" "main" {
-  cidr_block = var.cidr_block
+resource "aws_vpc" "this" {
+    cidr_block = var.vpc_cidr
 }
 
 resource "aws_subnet" "public" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.public_cidr
-  map_public_ip_on_launch = true
+    availability_zone = "us-west-2a"
+    cidr_block = var.public_subnet_cidr
+    vpc_id = aws_vpc.this.id
 }
 
 resource "aws_subnet" "private" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.private_cidr
+    availability_zone = "us-west-2a"
+    cidr_block = var.private_subnet_cidr
+    vpc_id = aws_vpc.this.id
 }
 
-resource "aws_security_group" "web" {
-  vpc_id = aws_vpc.main.id
+output "vpc_id" {
+    value = aws_vpc.this.id
+}
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+output "public_subnet_id" {
+    value = aws_subnet.public.id
+}
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+output "private_subnet_id" {
+    value = aws_subnet.private.id
 }
