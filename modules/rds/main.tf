187f422d3ae48806833141618644da6a60b119fe
Ahmed Elwakf <amtawfik140@gmail.com>
2024-07-12 19:14:58 +0300

    Update rds main.tf

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,24 +1,18 @@
-resource "aws_db_instance" "default" {
-  allocated_storage    = 20
-  engine               = "postgres"
-  engine_version       = "13.3"
-  instance_class       = "db.t2.micro"
-  name                 = var.db_name
-  username             = var.db_username
-  password             = var.db_password
-  db_subnet_group_name = aws_db_subnet_group.default.name
-  vpc_security_group_ids = [var.security_group_id]
-
-  storage_encrypted    = true
-
-  skip_final_snapshot = true
+resource "aws_db_subnet_group" "this" {
+    description = "RDS subnet group"
+    name = "rds-subnet-group"
+    subnet_ids = [var.subnet_id]
 }
 
-resource "aws_db_subnet_group" "default" {
-  name       = "main"
-  subnet_ids = [var.private_subnet_id]
+resource "aws_db_instance" "this" {
+    db_subnet_group_name = aws_db_subnet_group.this.name
+    engine = "postgres"
+    instance_class = var.rds_instance_type
+    password = var.rds_password
+    username = var.rds_username
+    vpc_security_group_ids = [var.vpc_security_group_id]
+}
 
-  tags = {
-    Name = "Main subnet group"
-  }
+output "db_instance_endpoint" {
+    value = aws_db_instance.this.endpoint
 }
