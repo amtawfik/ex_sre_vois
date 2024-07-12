89af89f7690390b92f42965cf2c1ec44acc325ac
Ahmed Elwakf <amtawfik140@gmail.com>
2024-07-12 22:14:45 +0300

    Update rds main.tf

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,11 +1,8 @@
 resource "aws_db_subnet_group" "this" {
     description = "RDS subnet group"
     name = "rds-subnet-group"
-    subnet_ids = [
-        aws_subnet.private.id,
-    ]
+    subnet_ids = [var.subnet_id]
 }
-
 
 resource "aws_db_instance" "this" {
     db_subnet_group_name = aws_db_subnet_group.this.name
@@ -13,11 +10,8 @@ resource "aws_db_instance" "this" {
     instance_class = var.rds_instance_type
     password = var.rds_password
     username = var.rds_username
-    vpc_security_group_ids = [
-        aws_security_group.ec2.id,
-    ]
+    vpc_security_group_ids = [var.vpc_security_group_id]
 }
-
 
 output "db_instance_endpoint" {
     value = aws_db_instance.this.endpoint
