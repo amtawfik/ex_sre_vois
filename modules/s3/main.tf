1ceba847d93c81836d1fab676ef3a7535ae73a1c
Ahmed Elwakf <amtawfik140@gmail.com>
2024-07-12 21:21:41 +0300

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,11 +1,11 @@
-resource "aws_s3_bucket" "bucket" {
-  bucket = var.bucket_name
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+resource "aws_s3_bucket" "this" {
+    acl = "private"
+    bucket = var.s3_bucket_name
+    server_side_encryption_configuration {
+        rule {
+            apply_server_side_encryption_by_default {
+                sse_algorithm = "AES256"
+            }
+        }
     }
-  }
 }
