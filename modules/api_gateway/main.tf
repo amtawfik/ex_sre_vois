faf2f25c67fc8739d9dcb82d9f5a1f70e9f9152f
Ahmed Elwakf <amtawfik140@gmail.com>
2024-07-12 23:13:58 +0300

    Update main.tf

--- modules/api_gateway/main.tf
+++ modules/api_gateway/main.tf
@@ -1,26 +1,26 @@
-resource "aws_api_gateway" "this" {
+resource "aws_api_gateway" "apigw" {
     description = "API Gateway"
     name = var.api_gateway_name
 }
 
-resource "aws_api_gateway_resource" "this" {
-    parent_id = aws_api_gateway.this.root_resource_id
+resource "aws_api_gateway_resource" "apigw-res" {
+    parent_id = aws_api_gateway.apigw.root_resource_id
     path_part = "my-resource"
-    rest_api_id = aws_api_gateway.this.id
+    rest_api_id = aws_api_gateway.apigw.id
 }
 
-resource "aws_api_gateway_method" "this" {
+resource "aws_api_gateway_method" "apigw-method" {
     authorization = "NONE"
     http_method = "GET"
-    resource_id = aws_api_gateway_resource.this.id
-    rest_api_id = aws_api_gateway.this.id
+    resource_id = aws_api_gateway_resource.apigw-res.id
+    rest_api_id = aws_api_gateway.apigw.id
 }
 
 resource "aws_api_gateway_integration" "this" {
-    http_method = aws_api_gateway_method.this.http_method
+    http_method = aws_api_gateway_method.apigw-method.http_method
     integration_http_method = "POST"
-    resource_id = aws_api_gateway_resource.this.id
-    rest_api_id = aws_api_gateway.this.id
+    resource_id = aws_api_gateway_resource.apigw-res.id
+    rest_api_id = aws_api_gateway.apigw.id
     type = "LAMBDA"
     uri = "arn:aws:apigateway:${aws_region}:lambda:path/2015-03-31/functions/${aws_lambda_function.this.arn}/invocations"
 }
