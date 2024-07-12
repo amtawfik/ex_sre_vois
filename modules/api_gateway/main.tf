fb9ccd574af0156a8a4004ecae832394c5e91487
Ahmed Elwakf <amtawfik140@gmail.com>
2024-07-12 21:13:57 +0300

    Update main.tf

--- modules/api_gateway/main.tf
+++ modules/api_gateway/main.tf
@@ -16,14 +16,15 @@ resource "aws_api_gateway_method" "method" {
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "integration" {
-  rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.resource.id
-  http_method             = aws_api_gateway_method.method.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.db_function.invoke_arn
+--resource "aws_api_gateway_integration" "this" {
+    http_method = aws_api_gateway_method.this.http_method
+    integration_http_method = "POST"
+    resource_id = aws_api_gateway_resource.this.id
+    rest_api_id = aws_api_gateway.this.id
+    type = "LAMBDA"
+    uri = "arn:aws:apigateway:${aws_region}:lambda:path/2015-03-31/functions/${aws_lambda_function.this.arn}/invocations"
 }
+
 
 resource "aws_lambda_permission" "api_gateway" {
   statement_id  = "AllowAPIGatewayInvoke"
