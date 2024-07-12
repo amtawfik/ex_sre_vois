2bbe6824a30d362aabd3c98ea5ad0e74d50bf0aa
Ahmed Elwakf <amtawfik140@gmail.com>
2024-07-12 19:09:55 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,27 +1,19 @@
 output "vpc_id" {
-  value = module.vpc.vpc_id
+    value = module.vpc.vpc_id
 }
 
 output "public_subnet_id" {
-  value = module.vpc.public_subnet_id
+    value = module.vpc.public_subnet_id
 }
 
 output "private_subnet_id" {
-  value = module.vpc.private_subnet_id
+    value = module.vpc.private_subnet_id
 }
 
 output "ec2_instance_id" {
-  value = module.ec2.instance_id
+    value = module.ec2.instance_id
 }
 
-output "rds_endpoint" {
-  value = module.rds.db_endpoint
-}
-
-output "lambda_function_arn" {
-  value = module.lambda.lambda_arn
-}
-
-output "api_gateway_endpoint" {
-  value = module.api_gateway.api_endpoint
+output "rds_instance_endpoint" {
+    value = module.rds.db_instance_endpoint
 }
