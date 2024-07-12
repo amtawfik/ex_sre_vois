7e184cfe9a173ef03b5d31c352f9641d19e64ca1
Ahmed Elwakf <amtawfik140@gmail.com>
2024-07-12 19:33:01 +0300

    Update lambda main.tf

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -1,58 +1,52 @@
-resource "aws_lambda_function" "db_function" {
-  filename         = "lambda_function_payload.zip"
-  function_name    = "db_function"
-  role             = aws_iam_role.lambda_exec.arn
-  handler          = "lambda_function.lambda_handler"
-  runtime          = "python3.8"
-
-  vpc_config {
-    subnet_ids         = var.private_subnet_ids
-    security_group_ids = var.security_group_ids
-  }
-
-  environment {
-    variables = {
-      DB_USERNAME = var.db_username
-      DB_PASSWORD = var.db_password
-      DB_HOST     = var.db_host
-      DB_NAME     = var.db_name
-    }
-  }
+resource "aws_lambda_function" "this" {
+    filename = "lambda_function_payload.zip"
+    function_name = var.lambda_function_name
+    handler = "index.handler"
+    role = aws_iam_role.lambda.arn
+    runtime = "nodejs14.x"
 }
 
-resource "aws_iam_role" "lambda_exec" {
-  name = "lambda_exec_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      },
-      Action = "sts:AssumeRole"
-    }]
-  })
+resource "aws_iam_role" "lambda" {
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+          {
+            Action = "sts:AssumeRole"
+            Principal = {
+              Service = "lambda.amazonaws.com"
+            }
+            Effect = "Allow"
+          }
+        ]
+      })
+    description = "IAM role for Lambda"
+    name = "lambda-role"
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
-  role       = aws_iam_role.lambda_exec.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+resource "aws_iam_role_policy_attachment" "lambda" {
+    policy_arn = aws_iam_policy.lambda.arn
+    role = aws_iam_role.lambda.name
 }
 
-resource "aws_iam_role_policy" "lambda_policy" {
-  name = "lambda_policy"
-  role = aws_iam_role.lambda_exec.id
 
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Action = [
-        "rds:*",
-        "s3:*"
-      ],
-      Resource = "*"
-    }]
-  })
+resource "aws_iam_policy" "lambda" {
+    description = "IAM policy for Lambda"
+    name = "lambda-policy"
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+          {
+            Action = [
+              "rds:DescribeDBInstances",
+              "rds:DescribeDBClusters"
+            ]
+            Resource = [
+              aws_db_instance.this.arn
+            ]
+            Effect = "Allow"
+          }
+        ]
+      })
 }
+
+
