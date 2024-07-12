6ca28a3191545c666a44a60352a093f884837f8c
Ahmed Elwakf <amtawfik140@gmail.com>
2024-07-12 22:12:25 +0300

    Update s3 main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -9,3 +9,29 @@ resource "aws_s3_bucket" "this" {
         }
     }
 }
+
+resource "aws_iam_policy" "ec2" {
+    description = "IAM policy for EC2"
+    name = "ec2-policy"
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+          {
+            Action = [
+              "s3:GetObject",
+              "s3:ListBucket"
+            ]
+            Resource = [
+              "arn:aws:s3:::${var.s3_bucket_name}",
+              "arn:aws:s3:::${var.s3_bucket_name}/*"
+            ]
+            Effect = "Allow"
+          }
+        ]
+      })
+}
+
+resource "aws_iam_role_policy_attachment" "ec2" {
+    policy_arn = aws_iam_policy.ec2.arn
+    role = aws_iam_role.ec2.name
+}
