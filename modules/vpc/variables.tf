ea005e6800139079243ecf266964876d52ab42ec
amtawfik <amtawfik140@gmail.com>
2024-07-12 12:31:07 +0300

    final 2

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,14 +1,11 @@
 variable "cidr_block" {
   description = "The CIDR block for the VPC"
-  value = "10.0.0.0/16"
 }
 
 variable "public_cidr" {
   description = "The CIDR block for the public subnet"
-  value = "10.0.1.0/24"
 }
 
 variable "private_cidr" {
   description = "The CIDR block for the private subnet"
-  value = "10.0.2.0/24"
 }
