a1e1454894fbf5f264d9473173fbc524f76e8b9e
Ahmed Elwakf <amtawfik140@gmail.com>
2024-07-12 22:13:59 +0300

    Update api main.tf

--- modules/api_gateway/main.tf
+++ modules/api_gateway/main.tf
@@ -9,27 +9,18 @@ resource "aws_api_gateway_resource" "this" {
     rest_api_id = aws_api_gateway.this.id
 }
 
-
-resource "aws_api_gateway_method" "method" {
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.resource.id
-  http_method   = "POST"
-  authorization = "NONE"
+resource "aws_api_gateway_method" "this" {
+    authorization = "NONE"
+    http_method = "GET"
+    resource_id = aws_api_gateway_resource.this.id
+    rest_api_id = aws_api_gateway.this.id
 }
 
---resource "aws_api_gateway_integration" "this" {
+resource "aws_api_gateway_integration" "this" {
     http_method = aws_api_gateway_method.this.http_method
     integration_http_method = "POST"
     resource_id = aws_api_gateway_resource.this.id
     rest_api_id = aws_api_gateway.this.id
     type = "LAMBDA"
     uri = "arn:aws:apigateway:${aws_region}:lambda:path/2015-03-31/functions/${aws_lambda_function.this.arn}/invocations"
-}
-
-
-resource "aws_lambda_permission" "api_gateway" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.db_function.function_name
-  principal     = "apigateway.amazonaws.com"
 }
