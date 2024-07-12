2e29c28420fa66650adb613c8b3103d7bf3fb31c
Ahmed Elwakf <amtawfik140@gmail.com>
2024-07-12 08:25:10 +0300

    Update api_gw main.tf

---
+++ modules/api_gateway/main.tf
@@ -0,0 +1,33 @@
+resource "aws_api_gateway_rest_api" "api" {
+  name        = "api_gateway"
+  description = "API Gateway for Lambda"
+}
+
+resource "aws_api_gateway_resource" "resource" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "resource"
+}
+
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.resource.id
+  http_method             = aws_api_gateway_method.method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.db_function.invoke_arn
+}
+
+resource "aws_lambda_permission" "api_gateway" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.db_function.function_name
+  principal     = "apigateway.amazonaws.com"
+}
